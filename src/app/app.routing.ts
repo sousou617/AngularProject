6a968cb0ae2bc4452842e5c0d4cc5f4892401c12
Cheryl Brown <38012775+sousou617@users.noreply.github.com>
2018-05-08 15:55:51 -0400

    Angular Project update 1.0

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,9 @@
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
-import {LoginComponent} from "./components/user/login/login.component";
-import {RegisterComponent} from "./components/user/register/register.component";
-import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/user/login/login.component';
 import { ProfileComponent } from './Components/user/profile/profile.component';
+import { RegisterComponent } from './Components/user/register/register.component';
+import { AppComponent } from './app.component';
 import { WebsiteNewComponent } from './Components/Website/website-new/website-new.component';
 import { WebsiteListComponent } from './Components/Website/website-list/website-list.component';
 import { WebsiteEditComponent } from './Components/Website/website-edit/website-edit.component';
@@ -19,12 +19,13 @@ import { WidgetYoutubeComponent } from './Components/Widget/widget-edit/widget-y
 // Import all other components here 
 
 const APP_ROUTES : Routes = [
+  { path : '' , component: LoginComponent},
   { path : 'login' , component: LoginComponent},
   { path : 'register' , component:RegisterComponent },
   { path : 'user/:uid' , component: ProfileComponent},
-  { path : 'user/:uid/website' , component: WebsiteListComponent}
-  { path : 'user/:uid/website/new' , component: WebsiteNewComponent}
-  { path : 'user/:uid/website/:wid' , component: WebsiteEditComponent}
+  { path : 'user/:uid/website' , component: WebsiteListComponent},
+  { path : 'user/:uid/website/new' , component: WebsiteNewComponent},
+  { path : 'user/:uid/website/:wid' , component: WebsiteEditComponent},
   { path : 'user/:uid/website/:wid/page', component : PageListComponent},
   { path : 'user/:uid/website/:wid/page/new', component : PageNewComponent},
   { path : 'user/:uid/website/:wid/page/:pid', component : PageEditComponent},
